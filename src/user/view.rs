c1076b2c350f62d351706a095496143b87caa50b
wwh <wuwh@ligeit.com>
2022-03-29 10:30:59 +0800

    betiful layout

--- src/user/view.rs
+++ src/user/view.rs
@@ -6,8 +6,8 @@ use crate::{
     },
 };
 use druid::{
-    widget::{Button, Flex, Label, TextBox, ViewSwitcher},
-    Widget, WidgetExt,
+    widget::{Button, CrossAxisAlignment, Flex, Label, MainAxisAlignment, TextBox, ViewSwitcher},
+    KeyOrValue, Widget, WidgetExt,
 };
 
 pub fn user_page() -> impl Widget<home::ViewModel> {
@@ -18,39 +18,48 @@ pub fn user_page() -> impl Widget<home::ViewModel> {
 }
 
 fn index_page(vm: &user::ViewModel) -> impl Widget<user::ViewModel> {
-    let mut col = Flex::column().with_child(
-        Flex::row()
-            .with_child(
-                TextBox::new()
-                    .with_placeholder("输入用户名")
-                    .lens(user::ViewModel::user_name),
-            )
-            .with_child(
-                TextBox::new()
-                    .with_placeholder("输入密码")
-                    .lens(user::ViewModel::password),
-            )
-            .with_child(
-                Button::new("新 增").on_click(|_ctx, vm: &mut user::ViewModel, _env| add_user(vm)),
-            )
-            .with_child(
-                Button::new("查 询")
-                    .on_click(|_ctx, vm: &mut user::ViewModel, _env| index_user(vm)),
-            ),
-    );
+    let mut col = Flex::column()
+        .cross_axis_alignment(CrossAxisAlignment::Start)
+        .with_child(
+            Flex::row()
+                .with_child(
+                    TextBox::new()
+                        .with_placeholder("输入用户名")
+                        .lens(user::ViewModel::user_name),
+                )
+                .with_default_spacer()
+                .with_child(
+                    TextBox::new()
+                        .with_placeholder("输入密码")
+                        .lens(user::ViewModel::password),
+                )
+                .with_flex_spacer(1.0)
+                .with_child(
+                    Button::new("新 增")
+                        .on_click(|_ctx, vm: &mut user::ViewModel, _env| add_user(vm)),
+                )
+                .with_default_spacer()
+                .with_child(
+                    Button::new("查 询")
+                        .on_click(|_ctx, vm: &mut user::ViewModel, _env| index_user(vm)),
+                ),
+        )
+        .with_spacer(KeyOrValue::Concrete(10.0));
 
     for item in vm.users.iter().enumerate() {
         let idx = item.0;
         let u = item.1;
-        col = col.with_child(
+        col = col.with_spacer(KeyOrValue::Concrete(6.0)).with_child(
             Flex::row()
+                .main_axis_alignment(MainAxisAlignment::SpaceBetween)
                 .with_child(Label::new(u.user_name.clone()))
                 .with_child(Label::new(u.password.clone()))
                 .with_child(
                     Button::new("删 除")
                         .on_click(move |_ctx, vm: &mut user::ViewModel, _env| delete_user(vm, idx)),
-                ),
+                )
+                .expand_width(),
         );
     }
-    col
+    col.padding((2.0, 10.0))
 }
